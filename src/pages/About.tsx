1e073831f9ad5cdbcab3da261bfc3b7c61ccb179
Artem <artem@MacBook-Air-Dara.local>
2022-10-05 22:32:54 +0600

    added about page and readme.md

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,7 +5,20 @@ const About: FC = () => {
         <div className="py-5">
             <div className='row justify-content-center'>
                 <div className='col-12 col-xxl-6 col-xl-6 col-lg-8 col-md-10'>
-                    <h1>About page</h1>
+                    <h1 className="mb-5">About page</h1>
+                    <p>Это простое приложения со списком задач. Приложение написано на React в функциональном стиле.</p>
+                    <p><strong>В этом приложении:</strong></p>
+                    <ul>
+                        <li>React v18 + TypeScript</li>
+                        <li>Авторизация через Google Firebase</li>
+                        <li>Работа с элементами через LocalStorage: добавление, редактирование, удаление</li>
+                        <li>Сделана сортировка и поиск по списку задач</li>
+                        <li>Настроен роутинг через React Router v6</li>
+                        <li>Компонентный подход</li>
+                        <li>Bootstrap v5</li>
+                        <li>React hooks</li>
+                        <li>Сборщик - Vite</li>
+                    </ul>
                 </div>
             </div>
         </div>
